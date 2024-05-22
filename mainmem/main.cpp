51fc2bf825aba4dea2b359825eb9f66b4bd80e9f
SoraYuki <sorayuki@live.com>
2024-05-23 00:29:42 +0800

    use multi thread / remove copy_n test

--- mainmem/main.cpp
+++ mainmem/main.cpp
@@ -42,14 +42,14 @@ struct FastMemcpy {
     }
 };
 
-template<class IMP>
-void DoTest(const char* name) {
-    const size_t size = 1024 * 1024 * 1024; // 1G
-    const size_t loop = 150;
+const size_t size = 64 * 1024 * 1024; // 64 MB
+const size_t loop = 300;
 
+template<class IMP>
+void DoTest(const char* name, std::atomic_int64_t* speedBps) {
     void* src = _aligned_malloc(size, 32);
     void* dst = _aligned_malloc(size, 32);
-    for(int i = 0; i < 5; ++i) // warm up
+    for(int i = 0; i < 3; ++i) // warm up
         memcpy(dst, src, size); // resolve page fault
 
     using namespace std::chrono;
@@ -63,24 +63,40 @@ void DoTest(const char* name) {
     
     printf("using %s: %g MB/S\n", name, speed);
 
+    speedBps->fetch_add(size * loop / (duration_cast<nanoseconds>(cost).count() / 1000000000.0));
+
     _aligned_free(src);
     _aligned_free(dst);
 }
 
 int main(int, char**){
-    const int parallel = 4;
+    const int parallel = std::thread::hardware_concurrency();
     std::vector<std::thread> t;
 
+    printf("thread: %d\n", parallel);
+
+    std::atomic_int64_t memcpyBps{0}, simdppBps{0}, fastmemcpyBps{0};
+
     for(int i = 0; i < parallel; ++i)
-        t.emplace_back([]() {
-            DoTest<STD>("std::memcpy");
-            DoTest<STD>("std::copy_n");
-            DoTest<SIMD>("simdpp");
-            DoTest<FastMemcpy>("FastMemcpy");
+        t.emplace_back([&]() {
+            DoTest<STD>("std::memcpy", &memcpyBps);
+            DoTest<SIMD>("simdpp", &simdppBps);
+            DoTest<FastMemcpy>("FastMemcpy", &fastmemcpyBps);
         });
 
     for(int i = 0; i < parallel; ++i)
         t[i].join();
+
+    printf("%s", "\n");
+    printf("%s", "Result\n");
+    printf("std::memcpy %g MB/S\n", memcpyBps.load() / 1048576.0);
+    printf("simdpp %g MB/S\n", simdppBps.load() / 1048576.0);
+    printf("FastMemcpy %g MB/S\n", fastmemcpyBps.load() / 1048576.0);
+
+    printf("%s", "End.\n");
+
+    int dummy;
+    scanf("%d", &dummy);
 
     return 0;
 }
